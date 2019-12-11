f247327721695ae94e69a48732ba61669c3c9c77
Jonathon Henderson <jrhenderson1988@hotmail.com>
2019-12-11 19:44:33 +0000

    Refactoring

--- src/main/kotlin/com/github/jrhenderson1988/adventofcode2019/Utils.kt
+++ src/main/kotlin/com/github/jrhenderson1988/adventofcode2019/Utils.kt
@@ -1,6 +1,12 @@
 package com.github.jrhenderson1988.adventofcode2019
 
 import java.io.File
+import kotlin.math.PI
+import kotlin.math.abs
+import kotlin.math.atan2
+
+const val R90 = PI / 2
+const val R360 = PI * 2
 
 fun readFileAsString(name: String): String {
     return File(name).readText()
@@ -8,4 +14,60 @@ fun readFileAsString(name: String): String {
 
 fun readFileAsLines(name: String): List<String> {
     return File(name).readLines()
+}
+
+// TODO - Parse grid
+// TODO - Parse intcodes
+
+// Calculate the Manhattan distance between two given points
+fun manhattanDistance(a: Pair<Int, Int>, b: Pair<Int, Int>) = abs(a.first - b.first) + abs(a.second - b.second)
+
+// Calculate the highest common factor (or greatest common divisor) of two integers using Euclid's algorithm
+fun hcf(a: Int, b: Int): Int = if (b == 0) a else hcf(b, a % b)
+
+// Calculate (in degrees) the angle of the line defined by two points assuming:
+// - a coordinate system where increasing X (first) moves right and increasing Y (second) moves down (top left to bottom
+//   right)
+// - The angle 0 (or 360) points straight up and the angle 90 degrees points to the right
+fun angle(a: Pair<Int, Int>, b: Pair<Int, Int>): Double {
+    val newA = Pair(a.first, a.second * -1)
+    val newB = Pair(b.first, b.second * -1)
+    val direction = Pair((newB.first - newA.first).toDouble(), (newB.second - newA.second).toDouble())
+    val angle = ((R360 - (atan2(direction.second, direction.first))) + R90) % R360
+
+    return angle * (180 / PI)
+}
+
+// Calculate a "delta" (x, y) pair from the two given (x, y) pairs such that applying the delta repeatedly to point a,
+// by adding the x values and y values will eventually result in reaching point b. The "delta" becomes effectively the
+// step.
+fun delta(a: Pair<Int, Int>, b: Pair<Int, Int>): Pair<Int, Int> {
+    val deltaX = abs(a.first - b.first)
+    val deltaY = abs(a.second - b.second)
+    val hcf = hcf(deltaX, deltaY)
+
+    return Pair(
+        if (a.first == b.first) 0 else (deltaX / hcf) * (if (a.first > b.first) -1 else 1),
+        if (a.second == b.second) 0 else (deltaY / hcf) * (if (a.second > b.second) -1 else 1)
+    )
+}
+
+// Calculate a set of points (integers) that exist between points a and b (exclusive).
+fun pointsBetween(a: Pair<Int, Int>, b: Pair<Int, Int>): Set<Pair<Int, Int>> {
+    val points = mutableSetOf<Pair<Int, Int>>()
+
+    val delta = delta(a, b)
+    val iterations = when {
+        delta.first != 0 -> (abs(a.first - b.first) / abs(delta.first)) - 1
+        delta.second != 0 -> (abs(a.second - b.second) / abs(delta.second)) - 1
+        else -> 0
+    }
+
+    var current = a
+    (0 until iterations).forEach { _ ->
+        current = Pair(current.first + delta.first, current.second + delta.second)
+        points.add(current)
+    }
+
+    return points.toSet()
 }
--- src/test/kotlin/com/github/jrhenderson1988/adventofcode2019/day10/SpaceTest.kt
+++ src/test/kotlin/com/github/jrhenderson1988/adventofcode2019/day10/SpaceTest.kt
@@ -88,54 +88,6 @@ class SpaceTest {
         }
 
     @Test
-    fun pointsBetween() =
-        mapOf(
-            Pair(Pair(4, 4), Pair(5, 5)) to emptySet(),
-            Pair(Pair(0, 0), Pair(0, 2)) to setOf(Pair(0, 1)),
-            Pair(Pair(1, 2), Pair(4, 5)) to setOf(Pair(2, 3), Pair(3, 4)),
-            Pair(Pair(0, 4), Pair(4, 0)) to setOf(Pair(1, 3), Pair(2, 2), Pair(3, 1)),
-            Pair(Pair(0, 0), Pair(2, 4)) to setOf(Pair(1, 2)),
-            Pair(Pair(0, 1), Pair(4, 9)) to setOf(Pair(1, 3), Pair(2, 5), Pair(3, 7)),
-            Pair(Pair(2, 2), Pair(0, 2)) to setOf(Pair(1, 2))
-        ).forEach { (input, expected) ->
-            assertEquals(expected, Space.pointsBetween(input.first, input.second))
-        }
-
-    @Test
-    fun hcf() =
-        mapOf(Pair(3, 6) to 3, Pair(4, 8) to 4, Pair(12, 8) to 4, Pair(12, 9) to 3, Pair(11, 23) to 1)
-            .forEach { (input, expected) -> assertEquals(expected, Space.hcf(input.first, input.second)) }
-
-    @Test
-    fun delta() =
-        mapOf(
-            Pair(Pair(4, 4), Pair(5, 5)) to Pair(1, 1),
-            Pair(Pair(0, 0), Pair(0, 2)) to Pair(0, 1),
-            Pair(Pair(1, 2), Pair(4, 5)) to Pair(1, 1),
-            Pair(Pair(0, 4), Pair(4, 0)) to Pair(1, -1),
-            Pair(Pair(0, 0), Pair(2, 4)) to Pair(1, 2),
-            Pair(Pair(0, 1), Pair(4, 9)) to Pair(1, 2),
-            Pair(Pair(1, 8), Pair(-3, 14)) to Pair(-2, 3)
-        ).forEach { (input, expected) ->
-            assertEquals(expected, Space.delta(input.first, input.second))
-        }
-
-    @Test
-    fun angle() =
-        mapOf(
-            Pair(Pair(2, 2), Pair(2, 1)) to 0.0,
-            Pair(Pair(2, 2), Pair(3, 2)) to 90.0,
-            Pair(Pair(2, 2), Pair(2, 3)) to 180.0,
-            Pair(Pair(2, 2), Pair(1, 2)) to 270.0,
-            Pair(Pair(1, 1), Pair(2, 0)) to 45.0,
-            Pair(Pair(1, 1), Pair(2, 2)) to 135.0,
-            Pair(Pair(1, 1), Pair(0, 2)) to 225.0,
-            Pair(Pair(1, 1), Pair(0, 0)) to 315.0
-        ).forEach { (input, expected) ->
-            assertEquals(expected, Space.angle(input.first, input.second), 0.1)
-        }
-
-    @Test
     fun calculateNthDestroyedAsteroid() =
         mapOf(
             Pair(TEST_GRID, 0) to Pair(3, 2),
