f62fb1136c2f361e12913468f2ee0ed976daad43
Jonathon Henderson <jrhenderson1988@hotmail.com>
2019-12-15 16:12:09 +0000

    WIP

--- src/main/kotlin/com/github/jrhenderson1988/adventofcode2019/day14/NanoFactory.kt
+++ src/main/kotlin/com/github/jrhenderson1988/adventofcode2019/day14/NanoFactory.kt
@@ -2,22 +2,21 @@ package com.github.jrhenderson1988.adventofcode2019.day14
 
 class NanoFactory(val reactions: Map<Chemical, List<Chemical>>) {
     fun calculateTotalOresRequiredToMakeFuel(): Int {
-        calculateTotalBaseElement(Chemical("FUEL", 1))
-        return 0
+        return calculateTotalRequiredBaseChemical(Chemical("A", 1))
     }
 
-    fun calculateTotalBaseElement(chemical: Chemical) {
+    fun calculateTotalRequiredBaseChemical(chemical: Chemical): Int {
         val ingredients = findIngredientsFor(chemical)
-        println("ingredients for $chemical: $ingredients")
-
         if (ingredients.isEmpty()) {
-            println("Base ingredient: $chemical")
-        } else {
-            for (ingredient in ingredients) {
-                calculateTotalBaseElement(ingredient)
-            }
+            return 1
         }
 
+        var total = 0
+        for (ingredient in ingredients) {
+            total += ingredient.quantity * calculateTotalRequiredBaseChemical(ingredient)
+        }
+
+        return total
     }
 
     private fun findIngredientsFor(chemical: Chemical) =
