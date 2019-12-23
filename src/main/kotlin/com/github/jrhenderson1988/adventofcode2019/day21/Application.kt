679142f89ae4122cb7aba599a9eb78a44751af07
Jonathon Henderson <jrhenderson1988@hotmail.com>
2019-12-23 18:32:16 +0000

    Day 21 part 2

--- src/main/kotlin/com/github/jrhenderson1988/adventofcode2019/day21/Application.kt
+++ src/main/kotlin/com/github/jrhenderson1988/adventofcode2019/day21/Application.kt
@@ -17,16 +17,17 @@ class Application {
         """
     )
 
-    // NOT A J: If the next tile is open, we MUST jump
-    // NOT C T: Invert C and put it in T i.e. if the 3rd tile is empty T becomes true
-    // AND D T: If D is closed (true) and C is open (false) then T stays true
-    // OR T J: If either T or J is true i.e. A is open OR C is open and D is closed, then jump
+    // D && (!A || !B || !C) && (E || H)
     fun part2(args: Array<String>) = SpringDroid(IntCodeComputer.parseProgram(readFileAsString(args.first()))).execute(
         """
-        NOT A J
-        NOT C T
-        AND D T
-        OR T J
+        OR A T
+        AND B T
+        AND C T
+        NOT T T
+        OR E J
+        OR H J
+        AND T J
+        AND D J
         RUN
         """
     )
