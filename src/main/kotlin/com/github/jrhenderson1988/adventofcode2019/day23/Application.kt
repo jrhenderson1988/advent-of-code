91a7953dc34386c92b97df53d164c9c3e5ad9b08
Jonathon Henderson <jrhenderson1988@hotmail.com>
2020-01-07 20:51:49 +0000

    Day 23 part 2

--- src/main/kotlin/com/github/jrhenderson1988/adventofcode2019/day23/Application.kt
+++ src/main/kotlin/com/github/jrhenderson1988/adventofcode2019/day23/Application.kt
@@ -3,7 +3,9 @@ package com.github.jrhenderson1988.adventofcode2019.day23
 import com.github.jrhenderson1988.adventofcode2019.readFileAsString
 
 class Application {
-    fun part1(args: Array<String>) {
-        Network(50, IntCodeComputer.parseProgram(readFileAsString(args.first()))).simulate()
-    }
+    fun part1(args: Array<String>) =
+        Network(50, IntCodeComputer.parseProgram(readFileAsString(args.first()))).firstYSentToAddress255()
+
+    fun part2(args: Array<String>) =
+        Network(50, IntCodeComputer.parseProgram(readFileAsString(args.first()))).firstYSentToResumeNetworkTwiceInARow()
 }
