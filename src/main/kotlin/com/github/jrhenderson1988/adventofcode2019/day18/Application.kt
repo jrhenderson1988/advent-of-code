5b7368bc7b52094d80f90a153f0604f9c0f933eb
Jonathon Henderson <jrhenderson1988@hotmail.com>
2020-01-19 21:07:01 +0000

    WIP: Day 18 part 2

--- src/main/kotlin/com/github/jrhenderson1988/adventofcode2019/day18/Application.kt
+++ src/main/kotlin/com/github/jrhenderson1988/adventofcode2019/day18/Application.kt
@@ -4,4 +4,5 @@ import com.github.jrhenderson1988.adventofcode2019.readFileAsString
 
 class Application {
     fun part1(args: Array<String>) = Vault.parse(readFileAsString(args.first())).shortestPathToCollectAllKeys()
+    fun part2(args: Array<String>) = QuadVault.parse(readFileAsString(args.first())).shortestPathToCollectAllKeys()
 }
