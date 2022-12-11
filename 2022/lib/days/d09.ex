00fed414bfc2ac8d24814cb3fadcb3249696d835
Jonathon Henderson <jrhenderson1988@hotmail.com>
2022-12-11 23:17:23 +0000

    Day 9 both parts

--- 2022/lib/days/d09.ex
+++ 2022/lib/days/d09.ex
@@ -1,12 +1,14 @@
 defmodule AoC.Days.D09 do
   def part_one(content) do
-    result = parse_instructions(content) |> simulate_and_count_tail_positions()
+    rope = make_rope(2)
+    result = parse_instructions(content) |> simulate_and_count_tail_positions(rope)
 
     {:ok, result}
   end
 
-  def part_two(_content) do
-    result = -1
+  def part_two(content) do
+    rope = make_rope(10)
+    result = parse_instructions(content) |> simulate_and_count_tail_positions(rope)
 
     {:ok, result}
   end
@@ -26,16 +28,11 @@ defmodule AoC.Days.D09 do
     end)
   end
 
-  defp simulate_and_count_tail_positions(instructions) do
-    {_, _, tail_positions} =
-      Enum.reduce(instructions, {{0, 0}, {0, 0}, MapSet.new([{0, 0}])}, fn instruction,
-                                                                           {head, tail,
-                                                                            tail_positions} ->
-        move_head(head, instruction)
-        |> Enum.reduce({head, tail, tail_positions}, fn new_head, {_, tail, tail_positions} ->
-          new_tail = move_tail(tail, new_head)
-          {new_head, new_tail, MapSet.put(tail_positions, new_tail)}
-        end)
+  defp simulate_and_count_tail_positions(instructions, rope) do
+    {_, tail_positions} =
+      Enum.reduce(instructions, {rope, MapSet.new([{0, 0}])}, fn instruction,
+                                                                 {rope, tail_positions} ->
+        move_rope(rope, instruction, tail_positions)
       end)
 
     MapSet.size(tail_positions)
@@ -56,35 +53,67 @@ defmodule AoC.Days.D09 do
     1..amount |> Enum.map(fn n -> {x + dx * n, y + dy * n} end)
   end
 
-  defp move_tail(current, head) do
+  defp is_adjacent({ax, ay}, {bx, by}) do
+    [{1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}]
+    |> Enum.any?(fn {dx, dy} -> ax + dx == bx and ay + dy == by end)
+  end
+
+  defp make_rope(size) do
+    0..(size - 1) |> Enum.map(fn _ -> {0, 0} end)
+  end
+
+  defp move_rope(rope, instruction, tail_positions) do
+    [head | rest] = rope
+
+    {head, rest, tail_positions} =
+      move_head(head, instruction)
+      |> Enum.reduce({head, rest, tail_positions}, fn new_head, {_, rest, tail_positions} ->
+        rest = move_rest(new_head, rest)
+        tail_positions = MapSet.put(tail_positions, Enum.at(rest, -1))
+
+        {new_head, rest, tail_positions}
+      end)
+
+    rope = [head | rest]
+
+    {rope, tail_positions}
+  end
+
+  defp move_rest(head, rest) do
+    {_, new_rest} =
+      Enum.reduce(rest, {head, []}, fn current, {neighbour, new_rest} ->
+        new_current = move_knot(current, neighbour)
+        new_rest = new_rest ++ [new_current]
+        {new_current, new_rest}
+      end)
+
+    new_rest
+  end
+
+  defp move_knot(current, neighbour) do
     cond do
-      is_neighbour(current, head) ->
+      is_adjacent(current, neighbour) ->
         current
 
       true ->
-        {tx, ty} = current
-        {hx, hy} = head
+        {cx, cy} = current
+        {nx, ny} = neighbour
 
         dx =
           cond do
-            tx < hx -> 1
-            tx > hx -> -1
+            cx < nx -> 1
+            cx > nx -> -1
             true -> 0
           end
 
         dy =
           cond do
-            ty < hy -> 1
-            ty > hy -> -1
+            cy < ny -> 1
+            cy > ny -> -1
             true -> 0
           end
 
-        {tx + dx, ty + dy}
+        {cx + dx, cy + dy}
     end
-  end
-
-  defp is_neighbour({ax, ay}, {bx, by}) do
-    [{1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}]
-    |> Enum.any?(fn {dx, dy} -> ax + dx == bx and ay + dy == by end)
   end
 end
--- 2022/test/days/d09_test.exs
+++ 2022/test/days/d09_test.exs
@@ -9,10 +9,17 @@ defmodule AoC.Days.D09Test do
       assert result == {:ok, 13}
     end
 
-    # test "part 2" do
-    #   input = Path.expand("./test/days/examples/d09.txt") |> File.read!()
-    #   result = AoC.Days.D09.part_two(input)
-    #   assert result == {:ok, 24933642}
-    # end
+    test "part 2" do
+      tests = [
+        {"./test/days/examples/d09.txt", 1},
+        {"./test/days/examples/d09_2.txt", 36},
+      ]
+
+      for {file, expected} <- tests do
+        input = Path.expand(file) |> File.read!()
+        result = AoC.Days.D09.part_two(input)
+        assert result == {:ok, expected}
+      end
+    end
   end
 end
