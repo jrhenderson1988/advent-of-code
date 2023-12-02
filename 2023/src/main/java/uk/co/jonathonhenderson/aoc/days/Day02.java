9b6146ccf3dcf154e678e21f967eb832c7baa27f
Jonathon Henderson <jrhenderson1988@hotmail.com>
2023-12-02 10:27:43 +0000

    2023 day 2

---
+++ 2023/src/main/java/uk/co/jonathonhenderson/aoc/days/Day02.java
@@ -0,0 +1,80 @@
+package uk.co.jonathonhenderson.aoc.days;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class Day02 extends Day {
+  @Override
+  public Optional<String> part1(String input) {
+    return parse(input).stream()
+        .filter(Game::isPossible)
+        .map(Game::id)
+        .reduce(Integer::sum)
+        .map(String::valueOf);
+  }
+
+  @Override
+  public Optional<String> part2(String input) {
+    return parse(input).stream().map(Game::power).reduce(Integer::sum).map(String::valueOf);
+  }
+
+  private List<Game> parse(String input) {
+    return input.trim().lines().map(Game::parse).toList();
+  }
+
+  record Game(int id, List<Handful> handfuls) {
+    private static final int MAX_RED = 12;
+    private static final int MAX_GREEN = 13;
+    private static final int MAX_BLUE = 14;
+
+    public static Game parse(String line) {
+      var parts = line.split(": ");
+      var id = Integer.parseInt(parts[0].split("Game")[1].trim());
+      var handfuls = Stream.of(parts[1].split(";")).map(Handful::parse).toList();
+      return new Game(id, handfuls);
+    }
+
+    public boolean isPossible() {
+      var max = this.handfuls().stream().reduce(Handful::max).orElseThrow();
+      return max.red() <= MAX_RED && max.green() <= MAX_GREEN && max.blue() <= MAX_BLUE;
+    }
+
+    public int power() {
+      var max = this.handfuls().stream().reduce(Handful::max).orElseThrow();
+      return max.red() * max.green() * max.blue();
+    }
+  }
+
+  record Handful(int red, int green, int blue) {
+
+    public static Handful parse(String line) {
+      return Stream.of(line.trim().split(","))
+          .map(String::trim)
+          .map(s -> s.split(" "))
+          .reduce(new Handful(0, 0, 0), Handful::build, Handful::combineHandfuls);
+    }
+
+    private static Handful build(Handful handful, String[] p) {
+      var qty = Integer.parseInt(p[0]);
+      var color = p[1].trim();
+      return switch (color) {
+        case "red" -> new Handful(handful.red() + qty, handful.green(), handful.blue());
+        case "green" -> new Handful(handful.red(), handful.green() + qty, handful.blue());
+        case "blue" -> new Handful(handful.red(), handful.green(), handful.blue() + qty);
+        default -> throw new IllegalStateException("Unexpected value: " + color);
+      };
+    }
+
+    private static Handful combineHandfuls(Handful h1, Handful h2) {
+      return new Handful(h1.red() + h2.red(), h1.green() + h2.green(), h1.blue() + h2.blue());
+    }
+
+    public Handful max(Handful other) {
+      return new Handful(
+          Math.max(this.red(), other.red()),
+          Math.max(this.green(), other.green()),
+          Math.max(this.blue(), other.blue()));
+    }
+  }
+}
---
+++ 2023/src/test/java/uk/co/jonathonhenderson/aoc/days/Day02Test.java
@@ -0,0 +1,28 @@
+package uk.co.jonathonhenderson.aoc.days;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+
+class Day02Test {
+  private final String INPUT =
+      """
+          Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+          Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+          Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+          Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+          Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green""";
+
+  private final Day02 day = new Day02();
+
+  @Test
+  void part1() {
+    assertEquals(Optional.of("8"), day.part1(INPUT));
+  }
+
+  @Test
+  void part2() {
+    assertEquals(Optional.of("2286"), day.part2(INPUT));
+  }
+}
