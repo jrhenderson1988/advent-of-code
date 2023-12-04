d691550a9eaa31401b9713baf4e85939b5ae5a03
Jonathon Henderson <jrhenderson1988@hotmail.com>
2023-12-04 21:29:32 +0000

    Minor refactor

--- 2023/src/main/java/uk/co/jonathonhenderson/aoc/days/Day04.java
+++ 2023/src/main/java/uk/co/jonathonhenderson/aoc/days/Day04.java
@@ -7,67 +7,77 @@ import java.util.stream.Stream;
 
 public class Day04 extends Day {
 
-  private final List<Card> cards;
+  private final Game game;
 
   public Day04(String input) {
-    this.cards = Card.parseCards(input);
+    this.game = Game.parse(input);
   }
 
   @Override
   public Optional<String> part1() {
-    return answer(cards.stream().map(Card::getValue).reduce(Integer::sum).orElseThrow());
+    return answer(game.totalValueOfCards());
   }
 
   @Override
   public Optional<String> part2() {
-    return answer(play().stream().map(Card::copies).reduce(Integer::sum).orElseThrow());
+    return answer(game.totalCardsAfterPlaying());
   }
 
-  private List<Card> play() {
-    var cards = this.cards.stream().collect(Collectors.toMap(Card::id, c -> c));
-    for (var id : this.cards.stream().map(Card::id).toList()) {
-      var card = cards.get(id);
-      var totalWinningNumbers = card.totalWinningNumbers();
-      var copies = card.copies();
-      for (var i = 1; i <= totalWinningNumbers; i++) {
-        var copiedId = id + i;
-        var copiedCard = cards.get(copiedId);
-        var newCard =
-            new Card(
-                copiedId,
-                copiedCard.winningNumbers(),
-                copiedCard.drawnNumbers(),
-                copiedCard.copies() + copies);
-        cards.put(copiedId, newCard);
-      }
+  private record Game(List<Card> cards) {
+    public static Game parse(String input) {
+      return new Game(input.trim().lines().map(Card::parse).toList());
     }
 
-    return cards.values().stream().toList();
+    public int totalValueOfCards() {
+      return cards.stream().map(Card::getValue).reduce(Integer::sum).orElseThrow();
+    }
+
+    public int totalCardsAfterPlaying() {
+      return play().stream().map(Card::copies).reduce(Integer::sum).orElseThrow();
+    }
+
+    private List<Card> play() {
+      var cards = this.cards.stream().collect(Collectors.toMap(Card::id, c -> c));
+      for (var id : cards.keySet()) {
+        var currentCard = cards.get(id);
+        var currentTotalWinningNumbers = currentCard.totalWinningNumbers();
+        var currentCopies = currentCard.copies();
+        for (var i = 1; i <= currentTotalWinningNumbers; i++) {
+          var copiedId = id + i;
+          var copiedCard = cards.get(copiedId);
+          var newCard =
+              new Card(
+                  copiedId,
+                  copiedCard.winningNumbers(),
+                  copiedCard.drawnNumbers(),
+                  copiedCard.copies() + currentCopies);
+          cards.put(copiedId, newCard);
+        }
+      }
+
+      return cards.values().stream().toList();
+    }
   }
 
   private record Card(
       int id, List<Integer> winningNumbers, List<Integer> drawnNumbers, int copies) {
-    public static List<Card> parseCards(String input) {
-      return input.trim().lines().map(Card::parseCard).toList();
-    }
 
-    private static Card parseCard(String line) {
+    private static Card parse(String line) {
       var parts = line.trim().split(":");
       var id = Integer.parseInt(parts[0].split("\\s+")[1].trim());
       var numbers =
           Stream.of(parts[1].split("\\s*\\|\\s*"))
               .map(String::trim)
-              .map(
-                  chunk ->
-                      Stream.of(chunk.split("\\s+"))
-                          .map(String::trim)
-                          .map(Integer::parseInt)
-                          .toList())
+              .map(Card::parseNumbers)
               .toList();
       if (numbers.size() != 2) {
         throw new IllegalArgumentException("Could not parse");
       }
       return new Card(id, numbers.get(0), numbers.get(1), 1);
+    }
+
+    private static List<Integer> parseNumbers(String chunk) {
+      return Stream.of(chunk.split("\\s+")).map(String::trim).map(Integer::parseInt).toList();
     }
 
     public int totalWinningNumbers() {
