f579cb326e6f142085ec89561f4b036d54d15644
Jonathon Henderson <jrhenderson1988@hotmail.com>
2023-12-18 21:37:47 +0000

    2023 Day 18 part 2

--- 2023/src/main/java/uk/co/jonathonhenderson/aoc/days/Day18.java
+++ 2023/src/main/java/uk/co/jonathonhenderson/aoc/days/Day18.java
@@ -1,10 +1,8 @@
 package uk.co.jonathonhenderson.aoc.days;
 
-import java.util.ArrayDeque;
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 import uk.co.jonathonhenderson.aoc.common.Direction;
 import uk.co.jonathonhenderson.aoc.common.Point;
 
@@ -23,19 +21,30 @@ public class Day18 extends Day {
 
   @Override
   public Optional<String> part2() {
-    return answer();
+    return answer(digPlan.calculateCapacityOfLagoonWithCorrectedPlan());
   }
 
-  private record Color(int red, int green, int blue) {
+  private record Color(String hex) {
     public static Color parse(String input) {
       var numbers = input.trim().replace("(", "").replace(")", "").replace("#", "").trim();
       if (numbers.length() != 6) {
         throw new IllegalArgumentException();
       }
-      var red = Integer.parseInt(numbers.substring(0, 2), 16);
-      var green = Integer.parseInt(numbers.substring(2, 4), 16);
-      var blue = Integer.parseInt(numbers.substring(4, 6), 16);
-      return new Color(red, green, blue);
+      return new Color(numbers);
+    }
+
+    public int amount() {
+      return Integer.parseInt(hex.substring(0, 5), 16);
+    }
+
+    public Direction direction() {
+      return switch (hex.substring(5, 6)) {
+        case "0" -> Direction.EAST;
+        case "1" -> Direction.SOUTH;
+        case "2" -> Direction.WEST;
+        case "3" -> Direction.NORTH;
+        default -> throw new IllegalArgumentException("Invalid direction");
+      };
     }
   }
 
@@ -57,6 +66,10 @@ public class Day18 extends Day {
         default -> throw new IllegalArgumentException("Invalid direction");
       };
     }
+
+    public Instruction correct() {
+      return new Instruction(color.direction(), color.amount(), color);
+    }
   }
 
   private record DigPlan(List<Instruction> instructions) {
@@ -64,78 +77,51 @@ public class Day18 extends Day {
       return new DigPlan(input.trim().lines().map(Instruction::parse).toList());
     }
 
-    public int calculateCapacityOfLagoon() {
-      var edges = calculateEdges();
-
-      var min = getMin(edges);
-      var newMin = Point.of(min.x() - 1, min.y() - 1);
-      var max = getMax(edges);
-      var newMax = Point.of(max.x() + 1, max.y() + 1);
-      var outer = fillOutside(edges, newMin, newMax);
-
-      var total = 0;
-      for (var y = min.y(); y <= max.y(); y++) {
-        for (var x = min.x(); x <= max.x(); x++) {
-          if (!outer.contains(Point.of(x, y))) {
-            total += 1;
-          }
-        }
-      }
-
-      return total;
+    public long calculateCapacityOfLagoon() {
+      var vertices = findVertices();
+      var area = calculateArea(vertices);
+      var perimeter = perimeterSize();
+      var interior = area - perimeter / 2 + 1;
+      return interior + perimeter;
     }
 
-    private Set<Point> fillOutside(Set<Point> edges, Point min, Point max) {
-      var cells = new HashSet<Point>();
-      var queue = new ArrayDeque<>(List.of(min));
-      while (!queue.isEmpty()) {
-        var curr = queue.poll();
-        if (curr.x() < min.x() || curr.x() > max.x() || curr.y() < min.y() || curr.y() > max.y()) {
-          continue;
-        } else if (cells.contains(curr)) {
-          continue;
-        } else if (edges.contains(curr)) {
-          continue;
-        }
-
-        cells.add(curr);
-        for (var dir : Direction.values()) {
-          var next = curr.translate(dir.delta());
-          queue.add(next);
-        }
-      }
-
-      return cells;
+    private long perimeterSize() {
+      return instructions.stream().map(Instruction::amount).reduce(Integer::sum).orElseThrow();
     }
 
-    private Set<Point> calculateEdges() {
+    public long calculateCapacityOfLagoonWithCorrectedPlan() {
+      var digPlan = new DigPlan(this.instructions.stream().map(Instruction::correct).toList());
+      return digPlan.calculateCapacityOfLagoon();
+    }
+
+    private long calculateArea(List<Point> vertices) {
+      var area = 0L;
+      for (var i = 0; i < vertices.size() - 1; i++) {
+        var curr = vertices.get(i);
+        var next = vertices.get(i + 1);
+        area += curr.x() * next.y() - next.x() * curr.y();
+      }
+      return Math.abs(area) / 2;
+    }
+
+    private List<Point> findVertices() {
       var curr = Point.of(0, 0);
 
-      var allEdges = new HashSet<>(Set.of(curr));
+      var vertices = new ArrayList<>(List.of(curr));
 
       for (var instruction : instructions) {
         var direction = instruction.direction();
         var delta = direction.delta();
-        for (var i = 0; i < instruction.amount(); i++) {
-          curr = curr.translate(delta);
-          allEdges.add(curr);
+        var actualDelta =
+            Point.of(delta.x() * instruction.amount(), delta.y() * instruction.amount());
+        curr = curr.translate(actualDelta);
+        if (!curr.equals(Point.of(0, 0))) {
+
+          vertices.add(curr);
         }
       }
 
-      return allEdges;
+      return vertices;
     }
-
-    private Point getMin(Set<Point> edges) {
-      return edges.stream()
-          .reduce((a, b) -> Point.of(Math.min(a.x(), b.x()), Math.min(a.y(), b.y())))
-          .orElseThrow();
-    }
-
-    private Point getMax(Set<Point> edges) {
-      return edges.stream()
-          .reduce((a, b) -> Point.of(Math.max(a.x(), b.x()), Math.max(a.y(), b.y())))
-          .orElseThrow();
-    }
-
   }
 }
--- 2023/src/test/java/uk/co/jonathonhenderson/aoc/days/Day18Test.java
+++ 2023/src/test/java/uk/co/jonathonhenderson/aoc/days/Day18Test.java
@@ -8,20 +8,20 @@ import org.junit.jupiter.api.Test;
 class Day18Test {
   private static final String INPUT =
       """
-      R 6 (#70c710)
-      D 5 (#0dc571)
-      L 2 (#5713f0)
-      D 2 (#d2c081)
-      R 2 (#59c680)
-      D 2 (#411b91)
-      L 5 (#8ceee2)
-      U 2 (#caa173)
-      L 1 (#1b58a2)
-      U 2 (#caa171)
-      R 2 (#7807d2)
-      U 3 (#a77fa3)
-      L 2 (#015232)
-      U 2 (#7a21e3)
+R 6 (#70c710)
+D 5 (#0dc571)
+L 2 (#5713f0)
+D 2 (#d2c081)
+R 2 (#59c680)
+D 2 (#411b91)
+L 5 (#8ceee2)
+U 2 (#caa173)
+L 1 (#1b58a2)
+U 2 (#caa171)
+R 2 (#7807d2)
+U 3 (#a77fa3)
+L 2 (#015232)
+U 2 (#7a21e3)
       """;
 
   @Test
@@ -31,6 +31,7 @@ class Day18Test {
 
   @Test
   void part2() {
-    assertEquals(Optional.empty(), new Day18(INPUT).part2());
+    assertEquals(Optional.of("952408144115"), new Day18(INPUT).part2());
+    // we're 2 below the result right now
   }
 }
