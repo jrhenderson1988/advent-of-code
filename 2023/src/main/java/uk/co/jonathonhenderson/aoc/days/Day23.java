bc48682675317e731c6a119ce488019394a1032d
Jonathon Henderson <jrhenderson1988@hotmail.com>
2023-12-27 21:28:20 +0000

    2023 Day 23 part 2

--- 2023/src/main/java/uk/co/jonathonhenderson/aoc/days/Day23.java
+++ 2023/src/main/java/uk/co/jonathonhenderson/aoc/days/Day23.java
@@ -1,13 +1,16 @@
 package uk.co.jonathonhenderson.aoc.days;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.IntStream;
 import uk.co.jonathonhenderson.aoc.common.Direction;
+import uk.co.jonathonhenderson.aoc.common.Pair;
 import uk.co.jonathonhenderson.aoc.common.Point;
 
 public class Day23 extends Day {
@@ -48,7 +51,19 @@ public class Day23 extends Day {
     }
   }
 
-  private record Grid(List<List<Cell>> cells) {
+  private static class Grid {
+    private final List<List<Cell>> cells;
+    private final Point start;
+    private final Point end;
+    private final Map<Point, List<Pair<Point, Integer>>> neighbourCache;
+
+    public Grid(List<List<Cell>> cells) {
+      this.cells = cells;
+      this.start = findStart();
+      this.end = findEnd();
+      this.neighbourCache = new HashMap<>();
+    }
+
     public static Grid parse(String input) {
       return new Grid(
           input.trim().lines().toList().stream()
@@ -70,29 +85,70 @@ public class Day23 extends Day {
       return dfs(start, end, this::neighboursOfIgnoringHills);
     }
 
-    private List<Point> neighboursOf(Point point) {
+    private List<Pair<Point, Integer>> neighboursOf(Point point) {
       return switch (cellAt(point)) {
         case OPEN -> Arrays.stream(Direction.values())
             .map(d -> d.delta().translate(point))
             .filter(p -> !cellAt(p).equals(Cell.WALL))
+            .map(p -> Pair.of(p, 1))
             .toList();
         case WALL -> throw new IllegalStateException("Should not be reachable");
-        case SLOPE_LEFT -> List.of(Direction.WEST.delta().translate(point));
-        case SLOPE_RIGHT -> List.of(Direction.EAST.delta().translate(point));
-        case SLOPE_DOWN -> List.of(Direction.SOUTH.delta().translate(point));
-        case SLOPE_UP -> List.of(Direction.NORTH.delta().translate(point));
+        case SLOPE_LEFT -> List.of(Pair.of(Direction.WEST.delta().translate(point), 1));
+        case SLOPE_RIGHT -> List.of(Pair.of(Direction.EAST.delta().translate(point), 1));
+        case SLOPE_DOWN -> List.of(Pair.of(Direction.SOUTH.delta().translate(point), 1));
+        case SLOPE_UP -> List.of(Pair.of(Direction.NORTH.delta().translate(point), 1));
       };
     }
 
-    public List<Point> neighboursOfIgnoringHills(Point point) {
-      return switch (cellAt(point)) {
-        case OPEN, SLOPE_LEFT, SLOPE_RIGHT, SLOPE_DOWN, SLOPE_UP -> Arrays.stream(
-                Direction.values())
-            .map(d -> d.delta().translate(point))
-            .filter(p -> !cellAt(p).equals(Cell.WALL))
-            .toList();
-        case WALL -> throw new IllegalStateException("Should not be reachable");
-      };
+    public List<Pair<Point, Integer>> neighboursOfIgnoringHills(Point point) {
+      if (cellAt(point).equals(Cell.WALL)) {
+        throw new IllegalStateException("Should not be reachable");
+      }
+
+      return neighbourCache.computeIfAbsent(
+          point,
+          (pt) -> {
+            var directions = getPossibleDirections(pt);
+            return directions.size() > 2
+                ? directions.stream().map(d -> Pair.of(d.delta().translate(pt), 1)).toList()
+                : directions.stream().map(dir -> seekNeighbour(pt, dir)).toList();
+          });
+    }
+
+    private List<Direction> getPossibleDirections(Point point) {
+      return Arrays.stream(Direction.values())
+          .filter(d -> !cellAt(d.delta().translate(point)).equals(Cell.WALL))
+          .toList();
+    }
+
+    private Pair<Point, Integer> seekNeighbour(Point point, Direction direction) {
+      var currentDirection = direction;
+      var currentPoint = point;
+      var steps = 0;
+
+      while (true) {
+        steps++;
+        currentPoint = currentDirection.delta().translate(currentPoint);
+
+        var possibleDirections = getPossibleDirections(currentPoint);
+        if (currentPoint.equals(start)) {
+          return Pair.of(start, steps);
+        } else if (currentPoint.equals(end)) {
+          return Pair.of(end, steps);
+        } else if (possibleDirections.size() > 2) {
+          return Pair.of(currentPoint, steps);
+        } else if (possibleDirections.size() == 1) {
+          throw new IllegalStateException("Should not be reachable");
+        }
+
+        var first = possibleDirections.getFirst();
+        var second = possibleDirections.getLast();
+        if (first.equals(currentDirection.opposite())) {
+          currentDirection = second;
+        } else {
+          currentDirection = first;
+        }
+      }
     }
 
     private Cell cellAt(Point point) {
@@ -109,7 +165,8 @@ public class Day23 extends Day {
       return line.get(x);
     }
 
-    private long dfs(Point start, Point end, Function<Point, List<Point>> neighbourFn) {
+    private long dfs(
+        Point start, Point end, Function<Point, List<Pair<Point, Integer>>> neighbourFn) {
       return dfs(start, end, Set.of(), 0, neighbourFn);
     }
 
@@ -118,18 +175,21 @@ public class Day23 extends Day {
         Point end,
         Set<Point> discovered,
         long length,
-        Function<Point, List<Point>> neighbourFn) {
+        Function<Point, List<Pair<Point, Integer>>> neighbourFn) {
       if (start.equals(end)) {
         return length;
       }
 
       var max = 0L;
-      for (var neighbour : neighbourFn.apply(start)) {
+      var neighbourPairs = neighbourFn.apply(start);
+      for (var neighbourPair : neighbourPairs) {
+        var neighbour = neighbourPair.first();
+        var cost = neighbourPair.second();
         if (!discovered.contains(neighbour)) {
           max =
               Math.max(
                   max,
-                  dfs(neighbour, end, discover(discovered, neighbour), length + 1, neighbourFn));
+                  dfs(neighbour, end, discover(discovered, neighbour), length + cost, neighbourFn));
         }
       }
       return max;
