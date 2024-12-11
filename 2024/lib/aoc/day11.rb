fb55c2846fe950be22173dd195ecb1d07fd7d328
Jonathon Henderson <jrhenderson1988@hotmail.com>
2024-12-11 20:55:07 +0000

    2024 day 11 part 2

--- 2024/lib/aoc/day11.rb
+++ 2024/lib/aoc/day11.rb
@@ -1,39 +1,48 @@
 module Aoc
   class Day11 < Day
     def part1
-      simulate(stones, 25).length
+      total_stones(blink_n_times(stones, 25))
     end
 
     def part2
-      simulate(stones, 75).length
+      total_stones(blink_n_times(stones, 75))
     end
 
-    def simulate(stones, times)
-      (0..times-1).reduce(stones) do |s, i|
-        puts("#{i}...")
-        blink(s, i+1)
-      end
-    end
-
-    def blink(stones, current)
-      new_stones = []
+    def blink_n_times(stones, times)
+      stone_counts = {}
 
       stones.each do |stone|
+        count = stone_counts[stone] || 0
+        stone_counts[stone] = count + 1
+      end
+
+      (0..times - 1).reduce(stone_counts) { |sc| blink(sc) }
+    end
+
+    def blink(stones_counts)
+      new_stone_counts = {}
+
+      stones_counts.each do |stone, count|
         if stone == 0
-          new_stones = new_stones + [1]
+          new_stone_counts[1] = (new_stone_counts[1] || 0) + count
         elsif stone.to_s.length % 2 == 0
           s = stone.to_s
           mid = (s.length / 2)
-          left = s[0..mid-1].to_i
+          left = s[0..mid - 1].to_i
           right = s[mid..].to_i
-          new_stones = new_stones + [left, right]
+
+          new_stone_counts[left] = (new_stone_counts[left] || 0) + count
+          new_stone_counts[right] = (new_stone_counts[right] || 0) + count
         else
-          new_stones = new_stones + [(stone * 2024)]
+          new_stone_counts[2024 * stone] = (new_stone_counts[2024 * stone] || 0) + count
         end
       end
 
-      puts("#{current}: #{new_stones.length}")
-      new_stones
+      new_stone_counts
+    end
+
+    def total_stones(stone_counts)
+      stone_counts.values.sum
     end
 
     def stones
