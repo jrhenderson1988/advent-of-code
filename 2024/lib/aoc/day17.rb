dfa5f8ae215a422f20089b92ac7c87178fb116ca
Jonathon Henderson <jrhenderson1988@hotmail.com>
2024-12-19 21:19:15 +0000

    2024 day 17 part 2 on hold -.-

--- 2024/lib/aoc/day17.rb
+++ 2024/lib/aoc/day17.rb
@@ -6,20 +6,84 @@ module Aoc
     end
 
     def part2
-      _, b, c = create_registers
+      # 2,4 -> bst(4) -> B = A % 8
+      # 1,1 -> bxl(1) -> B = B ^ 1
+      # 7,5 -> cdv(5) -> C = A / (2 ^ B)
+      # 0,3 -> adv(3) -> A = A / (2 ^ 3)
+      # 1,4 -> bxl(4) -> B = B ^ 4
+      # 4,4 -> bxc(4) -> B = B ^ C
+      # 5,5 -> out(5) -> OUTPUT B % 8
+      # 3,0 -> jnz(0) -> IF A == 0 HALT! ELSE IP = 0
 
-      i = 0
-      while true
-        # puts(i)
-        registers = [i, b, c]
-        output = execute_program(registers)
-        if output == program
-          return i
-        end
-        i += 1
-      end
+      # Translates to:
+      #
+      # while a != 0
+      #   B = A % 8
+      #   B = B ^ 1
+      #   C = A / (2 ^ B)
+      #   A = A / (2 ^ 3)
+      #   B = B ^ 4
+      #   B = B ^ C
+      #   OUTPUT B % 8
 
-      i
+      # In Ruby (refactored):
+      #
+      # a = 30886132
+      # b = 0
+      # c = 0
+      # output = []
+      # while a != 0
+      #   b = (a % 8) ^ 1
+      #   c = a / (2 ** b)
+      #   a = a / 8
+      #   b = b ^ 4 ^ c
+      #   output.append(b % 8)
+      # end
+
+      puts(output.join(","))
+
+      # _, b, c = create_registers
+      #
+      # puts(program.length)
+      #
+      # # Observation: the output increases by 1 character when i reaches 8 times the previous
+      # # time the output increased by 1 character. We could calculate the point to start based on
+      # # the size of the program output (assuming the same happens in the real output).
+      # #
+      # # e.g.
+      # # output size is 1 character long when i is 0
+      # # output size is 2 characters long when i is 8
+      # # output size is 3 characters long when i is 64
+      # # output size is 4 characters long when i is 512
+      # # output size is 5 characters long when i is 4096
+      # # output size is 6 characters long when i is 32768
+      # #
+      # # Given the above, we could start at 8^{<desired output size> - 1}
+      # # in this case, 8^(16-1) = 8^15 = 35184372088832
+      # i = 8 ** (program.length - 1)
+      #
+      # curr_output_size = 0
+      # current_first_char = nil
+      # while true
+      #   # puts(i)
+      #   registers = [i, b, c]
+      #   output = execute_program(registers)
+      #   if output.length != curr_output_size
+      #     curr_output_size = output.length
+      #     puts("new output size: #{curr_output_size} (#{i})")
+      #     if output[0] != current_first_char
+      #       current_first_char = output[0]
+      #       puts("first char changed: #{current_first_char} (#{i})")
+      #     end
+      #   end
+      #
+      #   if output == program
+      #     return i
+      #   end
+      #   i += 1
+      # end
+      #
+      # i
     end
 
     def execute_program(registers)
