da1dc9a1df2c30e9a5acb2fac8ca2af2d2fd0824
Jonathon Henderson <jrhenderson1988@hotmail.com>
2025-01-10 18:20:41 +0000

    2024 Day 21 both parts

--- 2024/lib/aoc/day21.rb
+++ 2024/lib/aoc/day21.rb
@@ -1,102 +1,82 @@
 require 'set'
 
 module Aoc
+  # This one was tough and I had to do quite a bit of reading on Reddit as to how to get this to
+  # work. I couldn't quite get part 1 working initially due to never quite getting the order of
+  # moves right. Part 2 was a bit easier once part 1 was down but was a bit of a rewrite, adding
+  # memoization, but it was still challenging. Inspired by some other great solutions.
   class Day21 < Day
-    D_PAD = [
-      %w[# ^ A],
-      %w[< v >],
-    ]
-
-    NUM_PAD = [
-      %w[7 8 9],
-      %w[4 5 6],
-      %w[1 2 3],
-      %w[# 0 A],
-    ]
+    NUM_PAD = "789456123#0A".chars
+    D_PAD = "#^A<v>".chars
 
     def part1
-      # this one was tricky... I couldn't get it working by doing path finding algorithms. I would
-      # always end up too low or too high, and sometimes I'd get the tests to pass but not my input.
-      # Had to resort to the Reddit threads to get some hints.
-      door_unlock_codes.map { |duc| complexity(duc, 2) }.sum
+      sum_of_complexities(door_unlock_codes, 2)
     end
 
     def part2
-      door_unlock_codes.map { |duc| complexity(duc, 25) }.sum
+      sum_of_complexities(door_unlock_codes, 25)
     end
 
-    def complexity(door_unlock_code, total_d_pad_robots)
-      num_pad_steps = path(door_unlock_code, num_pad_key_to_position, num_pad_position_to_key)
-      result = (0...total_d_pad_robots).reduce(num_pad_steps) {
-        |steps, _| path(steps, d_pad_key_to_position, d_pad_position_to_key)
-      }
+    def sum_of_complexities(door_unlock_codes, total_robots)
+      door_unlock_codes.map { |duc| complexity(duc, total_robots) }.sum
+    end
 
-      result.length * digits_of_door_unlock_code(door_unlock_code)
+    def complexity(door_unlock_code, total_robots)
+      digits = digits_of_door_unlock_code(door_unlock_code)
+      digits.to_i * length_of_shortest_sequence(digits, total_robots)
+    end
+
+    def path(a, b)
+      pad = choose_pad(a, b)
+      ay, ax = pad.index { |c| c == a }.divmod(3)
+      by, bx = pad.index { |c| c == b }.divmod(3)
+      s = repeat(">", bx - ax) + repeat("v", by - ay) + repeat("^", ay - by) + repeat("<", ax - bx)
+      if pad[ay * 3 + bx] == "#" || pad[by * 3 + ax] == "#"
+        s
+      else
+        s.reverse
+      end
+    end
+
+    def length_of_shortest_sequence(door_unlock_code, total_robots)
+      @complexity_p2_cache ||= {}
+
+      unless @complexity_p2_cache.member?([door_unlock_code, total_robots])
+        @complexity_p2_cache[[door_unlock_code, total_robots]] =
+          if total_robots < 0
+            door_unlock_code.length + 1
+          else
+            steps(door_unlock_code)
+              .map { |a, b| length_of_shortest_sequence(path(a, b), total_robots - 1) }
+              .sum
+          end
+      end
+
+      @complexity_p2_cache[[door_unlock_code, total_robots]]
+    end
+
+    def repeat(str, n)
+      n >= 0 ? str * n : ""
+    end
+
+    def choose_pad(a, b)
+      [NUM_PAD, D_PAD].find { |pad| pad.include?(a) && pad.include?(b) }
+    end
+
+    # "029" -> [["A", "0"], ["0", "2"], ["2", "9"], ["9", "A"]]
+    def steps(door_unlock_code)
+      start_a = "A#{door_unlock_code}".chars
+      end_a = "#{door_unlock_code}A".chars
+
+      start_a.zip(end_a)
     end
 
     def digits_of_door_unlock_code(door_unlock_code)
-      door_unlock_code.filter { |ch| ch.match(/^\d$/) }.join("").to_i
+      door_unlock_code.filter { |ch| ch.match(/^\d$/) }.join("")
     end
 
     def door_unlock_codes
       @door_unlock_codes ||= lines.map { |l| l.strip.chars }
-    end
-
-    def path(keys, key_to_position, position_to_key)
-      to_press = (['A'] + keys)
-
-      (1...to_press.length)
-        .map { |i| steps(to_press[i - 1], to_press[i], key_to_position, position_to_key) }
-        .reduce { |a, b| a + b }
-    end
-
-    def steps(source, target, key_to_position, position_to_key)
-      source_x, source_y = key_to_position[source]
-      target_x, target_y = key_to_position[target]
-      delta_x = target_x - source_x
-      delta_y = target_y - source_y
-      horizontal_steps = (delta_x < 0 ? ["<"] : [">"]) * delta_x.abs
-      vertical_steps = (delta_y < 0 ? ["^"] : ["v"]) * delta_y.abs
-
-      steps_to_take =
-        if delta_x > 0 && position_to_key.member?([source_x, target_y])
-          # we're moving right AND the vertical position that we're moving to (from the source) is
-          # in the grid
-          vertical_steps + horizontal_steps
-        elsif position_to_key.member?([target_x, source_y])
-          # the horizontal position we're moving to is in the grid
-          horizontal_steps + vertical_steps
-        else
-          # we're moving either left, or only vertically AND moving horizontally first would result
-          # in moving out of the grid
-          vertical_steps + horizontal_steps
-        end
-
-      steps_to_take + ["A"]
-    end
-
-    def num_pad_key_to_position
-      @num_pad_key_to_position ||=
-        (0...NUM_PAD.length).flat_map { |y| (0...NUM_PAD[y].length).map { |x| [NUM_PAD[y][x], [x, y]] } }
-                            .reject { |entry| entry[0] == '#' }
-                            .to_h
-    end
-
-    def num_pad_position_to_key
-      @num_pad_position_to_key ||=
-        num_pad_key_to_position.entries.map { |entry| [entry[1], entry[0]] }.to_h
-    end
-
-    def d_pad_key_to_position
-      @d_pad_key_to_position ||=
-        (0...D_PAD.length).flat_map { |y| (0...D_PAD[y].length).map { |x| [D_PAD[y][x], [x, y]] } }
-                          .reject { |entry| entry[0] == '#' }
-                          .to_h
-    end
-
-    def d_pad_position_to_key
-      @d_pad_position_to_key ||=
-        d_pad_key_to_position.entries.map { |entry| [entry[1], entry[0]] }.to_h
     end
   end
 end
