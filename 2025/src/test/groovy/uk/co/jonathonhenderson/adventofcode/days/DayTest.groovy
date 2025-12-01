4d4e49eec300d4710a27aa217fee79b3f645847c
Jonathon Henderson <jrhenderson1988@hotmail.com>
2025-12-01 17:13:37 +0000

    refactor

---
+++ 2025/src/test/groovy/uk/co/jonathonhenderson/adventofcode/days/DayTest.groovy
@@ -0,0 +1,47 @@
+package uk.co.jonathonhenderson.adventofcode.days
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals
+
+class DayTest {
+    def expectPart1(String expected) {
+        expectPart1(input() as String, expected)
+    }
+
+    def expectPart1(String content, String expected) {
+        expectPart1(createSubjectUnderTest(content), expected)
+    }
+
+    def expectPart1(Day day, String expected) {
+        def result = day.part1()
+        assertEquals(expected, result)
+    }
+
+    def expectPart2(String expected) {
+        expectPart2(input() as String, expected)
+    }
+
+    def expectPart2(String content, String expected) {
+        expectPart2(createSubjectUnderTest(content), expected)
+    }
+
+    def expectPart2(Day day, String expected) {
+        def result = day.part2()
+        assertEquals(expected, result)
+    }
+
+    Day createSubjectUnderTest(String content) {
+        def testClassName = this.getClass().name
+        if (!testClassName.endsWith("Test")) {
+            throw new IllegalStateException("Unexpected class name '${testClassName}'")
+        }
+        def clazz = Class.forName(testClassName.substring(0, testClassName.length() - 4))
+        def constructor = clazz.getDeclaredConstructor(String.class)
+
+        constructor.newInstance(content) as Day
+    }
+
+    def input() {
+        throw new IllegalStateException("Input must be provided")
+    }
+}
